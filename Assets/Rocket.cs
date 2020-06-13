1c1ce26c43cf051a055cf71c29b6b10953f8ba3a
cptran777 <cptran14@gmail.com>
2020-06-12 17:50:24 -0700

    Adds audio source to rocket component

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -3,11 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rocket : MonoBehaviour
-{
+public class Rocket : MonoBehaviour {
+    /**
+     * Reference to the rigidbody object that is attached to the same game object as this script
+     */
+    Rigidbody rigidBody;
+    /**
+     * Reference to the rocket thrust audio sound that is attached to the same game object as this
+     * script
+     */
+    AudioSource rocketThrustAudio;
+
     // Start is called before the first frame update
     void Start() {
-        
+        rigidBody = GetComponent<Rigidbody>();
+        rocketThrustAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -17,13 +27,18 @@ public class Rocket : MonoBehaviour
 
     private void ProcessInput() {
         if (Input.GetKey(KeyCode.Space)) {
-            print("Space pressed");
-        } 
+            rigidBody.AddRelativeForce(Vector3.up);
+            if (!rocketThrustAudio.isPlaying) {
+                rocketThrustAudio.Play();
+            }
+        } else {
+            rocketThrustAudio.Stop();
+        }
         
         if (Input.GetKey(KeyCode.A)) {
-            print("Going left");
+            transform.Rotate(Vector3.forward);
         } else if (Input.GetKey(KeyCode.D)) {
-            print("Going right");
+            transform.Rotate(-Vector3.forward);
         }
     }
 }
